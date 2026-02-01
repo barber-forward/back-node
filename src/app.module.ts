2cb3605f96519323f1b3d2003f78139d417b1c97
tiago-forward <devtiagoforward@gmail.com>
2026-02-01 14:22:14 -0300

    feat: add @nestjs/config for environment configuration and validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { PrismaService } from './prisma/prisma.service'
 import { CreateAccountClientController } from './controllers/create-account-client.controller'
+import { envSchema } from './env'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+  ],
   controllers: [CreateAccountClientController],
   providers: [PrismaService],
 })
