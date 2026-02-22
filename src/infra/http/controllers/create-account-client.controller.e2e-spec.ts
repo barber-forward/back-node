9a0d24bea746f7d820162a3f20db313cf9a633b7
tiago-forward <devtiagoforward@gmail.com>
2026-02-22 14:32:18 -0300

    feat: add end-to-end tests for CreateAccountClientController

---
+++ src/infra/http/controllers/create-account-client.controller.e2e-spec.ts
@@ -0,0 +1,45 @@
+import request from 'supertest'
+import { PrismaService } from '@/infra/prisma/prisma.service'
+import { INestApplication } from '@nestjs/common'
+import { AppModule } from '@/infra/app.module'
+import { Test } from '@nestjs/testing'
+
+describe('CreateAccountClientController (E2E)', () => {
+  let app: INestApplication
+  let prisma: PrismaService
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+
+    prisma = moduleRef.get(PrismaService)
+
+    await app.init()
+  })
+
+  test('[POST] /auth/register', async () => {
+    const response = await request(app.getHttpServer())
+      .post('/auth/register')
+      .send({
+        name: 'Tiago Teste',
+        email: 'tiagoteste@example.com',
+        password: '123456',
+      })
+
+    expect(response.statusCode).toBe(201)
+
+    const userOnDatebase = await prisma.client.findUnique({
+      where: {
+        email: 'tiagoteste@example.com',
+      },
+    })
+
+    expect(userOnDatebase).toBeTruthy()
+    expect(response.body).toEqual({
+      message: expect.any(String),
+    })
+  })
+})
