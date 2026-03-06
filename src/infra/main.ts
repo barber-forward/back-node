59d95132bea2ecb288a57ce15399d707e36acef4
tiago-forward <devtiagoforward@gmail.com>
2026-03-06 09:22:58 -0300

    feat: habilitar CORS com configuração de origem e credenciais

--- src/infra/main.ts
+++ src/infra/main.ts
@@ -9,6 +9,13 @@ import { ResponseInterceptor } from '../common/interceptors/response.interceptor
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
+  app.enableCors({
+    origin: (origin, callback) => {
+      callback(null, origin ?? '*')
+    },
+    credentials: true,
+  })
+
   app.use(cookieParser())
 
   app.setGlobalPrefix('api')
